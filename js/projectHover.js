c999980f34edd7d0bab0e8ec2db188e0f7231a6a
Taylor Wilkinson <tjwilkinsonn@gmail.com>
2018-12-17 23:58:08 -0800

    Added Times Past project page, and updated Projects preview page

---
+++ js/projectHover.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  $('#prj-container-1').hover(function() {
+    $('#proj-header-1').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-1').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-2').hover(function() {
+    $('#proj-header-2').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-2').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-3').hover(function() {
+    $('#proj-header-3').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-3').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-4').hover(function() {
+    $('#proj-header-4').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-4').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-5').hover(function() {
+    $('#proj-header-5').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-5').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-6').hover(function() {
+    $('#proj-header-6').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-6').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-7').hover(function() {
+    $('#proj-header-7').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-7').css('color', '#FFFFFF');
+  });
+});
+
+$(document).ready(function(){
+  $('#prj-container-8').hover(function() {
+    $('#proj-header-8').css('color', '#88BBD6');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#proj-header-8').css('color', '#FFFFFF');
+  });
+});
