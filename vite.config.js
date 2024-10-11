a06396e10f19d73cfad0b9dbb67ef03f9bf552b6
TaylorWilkinson <tjwilkinsonn@gmail.com>
2024-10-11 12:15:20 -0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
