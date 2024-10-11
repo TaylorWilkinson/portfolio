641ccfd77f93f4a5fa3d7bcae27dc34bbe081d32
TaylorWilkinson <tjwilkinsonn@gmail.com>
2024-10-11 10:56:26 -0700

    Revert to / base

--- taylors-portfolio-app/vite.config.js
+++ taylors-portfolio-app/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/taylors-portfolio-app/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
