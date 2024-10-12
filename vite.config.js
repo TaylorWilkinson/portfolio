a452cfce89c84a0c8272fb1013803dfa5a48384b
TaylorWilkinson <tjwilkinsonn@gmail.com>
2024-10-11 17:45:50 -0700

    Only a slash in publicPath

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  publicPath: '/dist/',
+  publicPath: '/',
   plugins: [vue()],
   resolve: {
     alias: {
